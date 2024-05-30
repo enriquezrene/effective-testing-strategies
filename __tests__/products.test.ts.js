d06c760873cc0d2bbb403b32ddf5d596c8a2d6bd
Rene Enriquez <rene.enriquez@clevertech.biz>
2024-05-30 18:00:20 -0500

    write a broken test

---
+++ __tests__/products.test.ts.js
@@ -0,0 +1,16 @@
+test('Return any product from the list when RECOMMEND ME is executed', () => {
+  // test setup
+
+  // function under testing execution
+  const recommendedGadget = recommendMe()
+
+  // expectations
+  expect(recommendedGadget).toEqual({
+    id: expect.any(Number),
+    name: expect.any(String)
+  })
+})
+
+function recommendMe() {
+
+}
