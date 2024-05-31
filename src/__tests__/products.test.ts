d8db2d7f22690fe95a3c59e0f2e6a31dbffd7910
Rene Enriquez <rene.enriquez@clevertech.biz>
2024-05-31 13:47:41 -0500

    describe for suites

--- src/__tests__/products.test.ts
+++ src/__tests__/products.test.ts
@@ -1,19 +1,25 @@
 import { ProductService } from '../product-service';
 
-it('When recommendMe is executed, it returns a gadget', () => {
-  const recommendedGadget = new ProductService().recommendMe()
+describe('When recommendMe is executed', ()=>{
 
-  expect(recommendedGadget).toEqual({
-    id: expect.any(Number),
-    name: expect.any(String)
+  it('it returns a gadget', () => {
+    const recommendedGadget = new ProductService().recommendMe()
+
+    expect(recommendedGadget).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String)
+    })
+  })
+
+  it('it gets a list of all the available products', () => {
+    // spies, jest built in feature
+    const findAvailableProductsFunction = jest.spyOn(ProductService.prototype, 'getAvailableProducts')
+
+    const recommendedGadget = new ProductService().recommendMe()
+
+    expect(findAvailableProductsFunction).toHaveBeenCalled()
   })
 })
 
-it('When recommendMe is executed, it gets a list of all the available products', () => {
-  // spies, jest built in feature
-  const findAvailableProductsFunction = jest.spyOn(ProductService.prototype, 'getAvailableProducts')
 
-  const recommendedGadget = new ProductService().recommendMe()
 
-  expect(findAvailableProductsFunction).toHaveBeenCalled()
-})
