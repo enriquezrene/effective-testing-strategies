6ebef901f39249f85dce056e721e6720a7934c21
Rene Enriquez <rene.enriquez@clevertech.biz>
2024-05-31 10:44:51 -0500

    introducing a new test case

--- src/__tests__/products.test.ts
+++ src/__tests__/products.test.ts
@@ -1,10 +1,16 @@
 import { ProductService } from '../product-service';
 
-it('When recommendMe is executed, it returns a gadget', ()=>{
+it('When recommendMe is executed, it returns a gadget', () => {
   const recommendedGadget = new ProductService().recommendMe()
 
   expect(recommendedGadget).toEqual({
     id: expect.any(Number),
     name: expect.any(String)
   })
+})
+
+it('When recommendMe is executed, it gets a list of all the available products', () => {
+  const recommendedGadget = new ProductService().recommendMe()
+
+  expect(findAvailableProductsFunction).toHaveBeenCalled()
 })
