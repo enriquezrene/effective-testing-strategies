cb89bbf0484de774ff59ef9a641977bb7c57eeec
Rene Enriquez <rene.enriquez@clevertech.biz>
2024-06-04 15:02:42 -0500

    Mocking response to avoid axios interaction

--- src/__tests__/products.test.ts
+++ src/__tests__/products.test.ts
@@ -1,9 +1,16 @@
-import { ProductService } from '../product-service';
+import { Gadget, ProductService } from '../product-service';
 
 describe('When recommend me is executed', () => {
 
   const findAvailableProductsFunction = jest.spyOn(ProductService.prototype, 'getAvailableProducts')
-  let recommendedGadget: any
+  findAvailableProductsFunction.mockImplementation(() => Promise.resolve([{
+    id: '1',
+    name: 'Apple iPad'
+  }, {
+    id: '2',
+    name: 'Google Pixel'
+  }]))
+  let recommendedGadget: Gadget
 
   beforeAll(async () => {
     recommendedGadget = await new ProductService().recommendMe()
@@ -11,7 +18,7 @@ describe('When recommend me is executed', () => {
 
   it('it returns a gadget', () => {
     expect(recommendedGadget).toEqual({
-      id: expect.any(Number),
+      id: expect.any(String),
       name: expect.any(String)
     })
   })
--- src/product-service.ts
+++ src/product-service.ts
@@ -2,18 +2,26 @@ import axios from 'axios';
 
 export class ProductService {
 
-  async recommendMe() {
+  async recommendMe(): Promise<Gadget> {
     const products = await this.getAvailableProducts()
     // return a random product here
     return {
-      id: 1,
+      id: '1',
       name: 'iPad'
     }
   }
-  async getAvailableProducts() {
+
+  async getAvailableProducts(): Promise<Gadget[]> {
     const products = await axios.get(`https://api.restful-api.dev/objects`)
     console.log(products.data)
     return products.data
   }
 
+}
+
+
+export type Gadget = {
+  id: string,
+  name: string,
+  data?: any
 }
