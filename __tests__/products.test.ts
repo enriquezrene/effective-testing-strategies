c33ca2e2b3ef9f6629caad055cfd7070f4bc5409
Rene Enriquez <rene.enriquez@clevertech.biz>
2024-06-05 00:47:15 -0500

    change test

--- __tests__/products.test.ts
+++ __tests__/products.test.ts
@@ -8,7 +8,7 @@ it('When recommendMe is executed, it returns a gadget', ()=>{
   const recommendedGadget = new ProductService().recommendMe()
 
   expect(recommendedGadget).toEqual({
-    id: expect.any(Number),
+    id: expect.any(String),
     name: expect.any(String)
   })
 })
